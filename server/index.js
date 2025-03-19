409486b1f081441dcc8d877c282023de82bc9ef8
Keshav Priyam <keshavpriyam@gmail.com>
2025-03-20 00:12:48 +0530

    build server confix module

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("test req");
+    console.table({req})
+});
+
+app.listen(3000,()=>{
+    console.log('Server on port 3000');
+})
