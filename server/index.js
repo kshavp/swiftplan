f08b155f9f7e0b36dee5c4da9e7b01c0883ee757
Keshav Priyam <keshavpriyam@gmail.com>
2025-03-20 08:36:09 +0530

    instantiate socket object and http server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import { Server } from 'socket.io';
+import http from 'http';
 
 const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+//PENDING CORS CONFIGURATION
+// const io = new Server(server, {
+//   cors: {
+//     origin: 'REACT_APP_CORS_ORIGIN',
+//   }
+// });
+
+const io = new Server(server);
 
 app.get('/',(req,res)=>{
     res.send("test req");
-    console.table({req})
+    // console.table({req});
 });
 
-app.listen(3000,()=>{
-    console.log('Server on port 3000');
-})
+
+server.listen(3000);
