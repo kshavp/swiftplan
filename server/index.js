acf33e33c76a0461770a14be02a86fd68149ee72
Keshav Priyam <keshavpriyam@gmail.com>
2025-03-24 09:27:07 +0530

    complete server code

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import { Server } from 'socket.io';
 import http from 'http';
-
+import path from 'path';
 const app = express();
 app.use(cors());
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 const server = http.createServer(app);
 
@@ -17,10 +22,19 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 
-app.get('/',(req,res)=>{
-    res.send("test req");
-    // console.table({req});
-});
+io.on("connection", function (socket) {
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", { id: socket.id, ...data })
+    })
 
+    socket.on("disconnect", function () {
+        io.emit("user-disconnect", { id: socket.id })
+    })
+})
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
 
 server.listen(3000);
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 function App() {
+
+  // useEffect(() => {
+
+  // })
+
   return (
     <>
-      <h1>Test</h1>
+      <h1 className='text-red-600'>Test</h1>
     </>
   )
 }
